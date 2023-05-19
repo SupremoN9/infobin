0f40bb0caa41a42c2ebf539669e706264e45ab6a
SupremoN9 <115044099+SupremoN9@users.noreply.github.com>
2023-05-18 19:25:35 -0600

    Update carding-killer.py

--- carding-killer.py
+++ carding-killer.py
@@ -1,24 +1,24 @@
 import requests
 from colorama import Fore, Style
 
-# URL base de la API
+
 url_base = "https://lookup.binlist.net/"
 
-# Función que toma el número de BIN como argumento y devuelve la información correspondiente
+
 def obtener_info_bin(bin_num):
-    # Concatenar el número de BIN con la URL base de la API
+   
     url = url_base + bin_num
 
-    # Establecer los headers para indicar que se espera una respuesta en formato JSON
+   
     headers = {
         "Accept-Version": "3",
         "Accept": "application/json"
     }
 
-    # Hacer la petición GET a la API
+  
     response = requests.get(url, headers=headers)
 
-    # Si la respuesta fue exitosa, extraer la información necesaria del JSON
+    
     if response.status_code == 200:
         data = response.json()
         return {
@@ -30,10 +30,10 @@ def obtener_info_bin(bin_num):
             "Estado de la tarjeta": "Válido" if response.status_code == 200 else "Inválido"
         }
     else:
-        # Si la respuesta no fue exitosa, devolver un diccionario vacío
+        
         return {}
 
-# Print inicial con información del creador y las redes sociales
+
 print(Fore.GREEN + Style.BRIGHT + """
 ==================================================
 |              Verificador de BINs                |
@@ -45,10 +45,10 @@ print(Fore.GREEN + Style.BRIGHT + """
 ==================================================
 """ + Style.RESET_ALL)
 
-# Solicitar al usuario el número de BIN
+
 bin_num = input("Ingresa los primeros 6 dígitos de tu tarjeta (BIN): ")
 
-# Obtener la información del BIN y mostrarla en pantalla
+
 info_bin = obtener_info_bin(bin_num)
 if info_bin:
     print("\n" + Fore.YELLOW + Style.BRIGHT + "La información del BIN es la siguiente:\n" + Style.RESET_ALL)
